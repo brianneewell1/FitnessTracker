9efda54419411409e56e37e5080e528adf5d4bdc
Brianne <brianne.ewell1@gmail.com>
2021-01-30 10:11:14 -0700

    updating apiroutes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -4,35 +4,47 @@ const Workout = require("../models/workout");
 
 module.exports = function(app) {
  //gets information for workouts page
-    router.get("/api/workouts", (req, res) => {
+    app.get("/api/workouts", (req, res) => {
         db.Workout.find({}).then(dbWorkout => {
+          dbWorkout.forEach(workout => {
+            var total = 0;
+            workout.exercises.forEach(e => {
+              total += e.duration;
+            });
+            workout.totalDuration = total;
+          });
             res.json(dbWorkout);
         })
         .catch(err => {
             res.status(400).json(err);
         });
-    })
+    });
    //range page
-    router.get("/api/workouts/range", ({}, res) => {
-      db.Workout.find({}).then((dbWorkout) => {
+    app.get("/api/workouts/range", (req, res) => {
+      db.Workout.find({}).then(dbWorkout => {
         res.json(dbWorkout);
       }).catch(err => {
         res.status(400).json(err);
       });
     });
     //create new workouts
-    router.post("/api/workouts/", (req, res) => {
-        db.Workout.create(req.body).then((dbWorkout) => {
-          res.json(dbWorkout);
-        }).catch(err => {
-            res.status(400).json(err);
-          });
-      });
+    app.post("/api/workouts/", ({ body }, res) => {
+    db.Workout.create(body).then((dbWorkout => {
+      res.json(dbWorkout);
+    })).catch(err => {
+      res.json(err);
+    });
+    });
+   
      //updates database
       app.put("/api/workouts/:id", (req, res) => {
-        db.Workout.findByIdAndUpdate(
-          { _id: req.params.id }, { exercises: req.body }
-        ).then((dbWorkout) => {
+        db.Workout.findOneAndUpdate(
+          { _id: req.params.id }, 
+          {
+          $inc: {totalDuration: req.body.duration},
+          $push: { exercises: req.body }  
+          },
+          {new: true}).then((dbWorkout) => {
           res.json(dbWorkout);
         }).catch(err => {
           res.status(400).json(err);
