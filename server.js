358b6218895bea4bdb52d67b82ce40c2b8fc3287
Brianne <brianne.ewell1@gmail.com>
2021-01-29 19:04:17 -0700

    updating connection

--- server.js
+++ server.js
@@ -28,6 +28,9 @@ mongoose.connect(
 app.use(require("./routes/htmlroutes"));
 app.use(require("./routes/apiroutes"));
 
-app.listen(PORT, () => {
+// app.listen(process.env.PORT || 3000, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
+app.listen(process.env.PORT || 3001, function() {
   console.log(`App running on port ${PORT}!`);
 });
