5fe04b892a0a2932057e1ac9d3b1459f423c0a61
Brianne <brianne.ewell1@gmail.com>
2021-03-06 09:59:27 -0700

    deploying

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Il0vecasey:Il0vecasey@cluster0.7hoz7.mongodb.net/fitness_db?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useFindAndModify: false
 });
